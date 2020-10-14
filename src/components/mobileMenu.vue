455afe2c4df40729c580bfe3bc888bedb81f38e7
michael.kuechler <michael.kuechler2@uzh.ch>
2020-10-14 18:29:16 +0200

    remove /article in url

--- src/components/mobileMenu.vue
+++ src/components/mobileMenu.vue
@@ -25,7 +25,7 @@
                         <div v-for="subLink in menuItem.dropDowns"
                              v-bind:key="subLink.index" class="subLink">
                             <router-link class="router-link"
-                                         :to="{path: '/articles/' + replaceWhiteSpaces(subLink.toLowerCase())}">
+                                         :to="{path:replaceWhiteSpaces(subLink.toLowerCase())}">
                                 <span v-on:click="activateMenuWindow()">{{ subLink }}</span>
                             </router-link>
                         </div>
@@ -65,7 +65,7 @@ export default {
             return str.replace(' ', '-').replace(' ', '-');
         },
         navigateToStandaloneComponent(componentString) {
-            this.router.push(`/articles/${this.replaceWhiteSpaces(componentString)}`);
+            this.router.push(`/${this.replaceWhiteSpaces(componentString)}`);
         },
         closeDrowns() {
             let dropDownsNumber = 0;
